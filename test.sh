ea7d3b8d2f2fde3504f268cdeac74f128cbc6ed1
Niklas Marion <niklas.marion@gmx.de>
2020-11-28 22:20:21 +0000

    feat: add article test cases

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ cd ~
 rm -rf PIB-PR1-Tests/
 git clone https://github.com/nimarion/PIB-PR1-Tests.git
 cp PRG-SPR/$1/*.java PIB-PR1-Tests/src/main/java/
-cd PIB-PR1-Tests && mvn clean surefire-report:report 
+cd PIB-PR1-Tests && mvn clean -Dtest=$2 surefire-report:report 
 if [ -n "$3" ]; then
   mvn site -DgenerateReports=false
   echo "Open report in browser"  
