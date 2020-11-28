1d315fe8054df684eead3ef92b36c5aa25548de1
Niklas Marion <niklas.marion@gmx.de>
2020-11-28 22:26:38 +0000

    fix: delete non used test classes

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ cd ~
 rm -rf PIB-PR1-Tests/
 git clone https://github.com/nimarion/PIB-PR1-Tests.git
 cp PRG-SPR/$1/*.java PIB-PR1-Tests/src/main/java/
+find PIB-PR1-Tests/src/test/java/ -type f ! -name $2 -delete
 cd PIB-PR1-Tests && mvn clean -Dtest=$2 surefire-report:report 
 if [ -n "$3" ]; then
   mvn site -DgenerateReports=false
