73ed3c4fa6fa43ec3a4b2949b0009d53d122419d
Niklas Marion <niklas.marion@gmx.de>
2020-11-28 19:54:46 +0000

    feat: generate html sitereport

--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@ cd ~
 rm -rf PIB-PR1-Tests/
 git clone https://github.com/nimarion/PIB-PR1-Tests.git
 cp PRG-SPR/$1/*.java PIB-PR1-Tests/src/main/java/
-cd PIB-PR1-Tests && mvn -Dtest=$2 test
+cd PIB-PR1-Tests && mvn clean surefire-report:report 
+if [ -n "$3" ]; then
+  mvn site -DgenerateReports=false
+  echo "Open report in browser"  
+  firefox ./target/site/surefire-report.html  
+fi     
