05e0579b25cd84b2a47d654afa560fb72cb67b65
Niklas Marion <niklas.marion@gmx.de>
2020-11-28 23:21:17 +0000

    fix: use portable maven version due to old target system

--- test.sh
+++ test.sh
@@ -2,11 +2,15 @@
 cd ~
 rm -rf PIB-PR1-Tests/
 git clone https://github.com/nimarion/PIB-PR1-Tests.git
-cp PRG-SPR/$1/*.java PIB-PR1-Tests/src/main/java/
-find PIB-PR1-Tests/src/test/java/ -type f ! -name $2 -delete
-cd PIB-PR1-Tests && mvn clean -Dtest=$2 surefire-report:report 
+cd PIB-PR1-Tests && wget https://gist.github.com/nimarion/4c17646d69cc8597d2ccf78196ffa8af/raw/43ba80ac96e49a7f430c0bec8ba11afec69bd3e1/apache-maven-3.6.3-bin.zip && unzip apache-maven-3.6.3-bin.zip 1> /dev/null
+M2_HOME=apache-maven-3.6.3
+M2=$M2_HOME/bin
+PATH=$M2:$PATH
+cp ~/PRG-SPR/$1/*.java ~/PIB-PR1-Tests/src/main/java/
+find ~/PIB-PR1-Tests/src/test/java/ -type f ! -name $2 -delete
+cd ~/PIB-PR1-Tests && mvn clean -Dtest=$2 surefire-report:report
 if [ -n "$3" ]; then
   mvn site -DgenerateReports=false
-  echo "Open report in browser"  
-  firefox ./target/site/surefire-report.html  
-fi     
+  echo "Open report in browser"
+  firefox ./target/site/surefire-report.html
+fi
