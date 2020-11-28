1d36854590da1a3c0322a46a78b84480585bde80
Niklas Marion <niklas.marion@gmx.de>
2020-11-28 15:32:59 +0000

    feat: delete existing git folder

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~
+rm -rf PIB-PR1-Tests/
 git clone https://github.com/nimarion/PIB-PR1-Tests.git
 cp PRG-SPR/$1/*.java PIB-PR1-Tests/src/main/java/
 cd PIB-PR1-Tests && mvn -Dtest=$2 test
